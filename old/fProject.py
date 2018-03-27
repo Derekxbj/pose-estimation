96de5647b7c8c59feb1ef11a84b617066aae548e
Derekxbj <bdxie@gwu.edu>
2018-03-27 18:19:25 -0400

    Wrap up the old codes

---
+++ old/fProject.py
@@ -0,0 +1,65 @@
+import numpy as np 
+from math import sin,cos
+
+def fProject(x, P_M, K):
+	# x = np.array([0,0,0,0,0,23])
+
+	# # # The point in the model's coordinate system(cm)
+	# P_M = np.array([[-5,5,5,-5],
+	#                [-5,-5,5,5],
+	#                [0,0,0,0],
+	#                [1,1,1,1]])
+
+
+	# # # Define camera parameters
+	# f = 504
+	# cx = 320
+	# cy = 240
+
+	# # # intrinsic parameter matrix
+	# K = np.array([[f,0,cx],
+	#               [0,f,cy],
+	#               [0,0,1]])
+
+	# Get pose parametes
+	ax = x[0]
+	ay = x[1]
+	az = x[2]
+	tx = x[3]
+	ty = x[4]
+	tz = x[5]
+
+	# Rotation matrix using Euler angle
+	Rx = np.array([[1,0,0],
+				  [0,cos(ax),-sin(ax)],
+				  [0,sin(ax),cos(ax)]])
+
+	Ry = np.array([[cos(ay),0,sin(ay)],
+				   [0,1,0],
+				   [-sin(ay),0,cos(ay)]])
+
+	Rz = np.array([[cos(az),-sin(az),0],
+				   [sin(az),cos(az),0],
+				   [0,0,1]])
+	R = (Rz.dot(Ry)).dot(Rx)
+
+
+	# Extrinsic camera matrix (rotation and translation)
+	T = np.array([tx , ty, tz])
+	Mext = np.hstack((R,T))
+
+
+	# Projects points
+	ph = (K.dot(Mext)).dot(P_M)
+	ph[0,:] = ph[0,:]/ph[2,:]
+	ph[1,:] = ph[1,:]/ph[2,:]
+	ph = ph[:2,:]
+
+	p = ph.reshape((8,1), order='F')
+	# p = p.round(decimals = 100)
+
+	return p
+
+
+
+
---
+++ old/image_position.py
@@ -0,0 +1,129 @@
+# from __future__ import print_function
+# import numpy as np
+import cv2
+from pandas import *
+from scipy.spatial import ConvexHull
+from numpy import linalg as LA
+
+def image_position(image):
+	# image = cv2.imread('image2.jpg')
+	image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	# cv2.imshow("Image", image)
+
+	kernel = np.ones((5,5), np.uint8)
+
+	(T, thresh) = cv2.threshold(image, 90, 255, cv2.THRESH_BINARY)
+	# cv2.imshow("Threshold Binary", thresh)
+	opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
+	# cv2.imshow("Imopen White", opening)
+
+	img, contours, hierarchy = cv2.findContours(opening, 
+						cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+
+	# print(len(contours))
+	blobsWhite = np.array([])
+	for i in range(0, len(contours)):
+		try:
+			cnt = contours[i]
+			M = cv2.moments(cnt)
+			# print(M)
+			# cx = int(M['m10']/M['m00'])
+			# cy = int(M['m01']/M['m00'])
+			cx = M['m10']/M['m00']
+			cy = M['m01']/M['m00']
+			# print(cx, cy)
+			if i == 0:
+				blobsWhite = np.concatenate((blobsWhite, np.array([cx,cy])))
+			else:
+				blobsWhite = np.vstack((blobsWhite,np.array([cx,cy])))
+			
+		except ZeroDivisionError as detail:
+			print ('Handling error:', detail)
+
+	# print(blobsWhite)
+
+	# I find that the openCV findContours function find all the 
+	# regionprops including black and white
+
+	# The fllowing code may helpful in the future.
+	# (T, threshInv) = cv2.threshold(image, 90, 255, cv2.THRESH_BINARY_INV)
+	# # cv2.imshow("Threshold Binary Inverse", threshInv)
+	# opening_black = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
+	# # cv2.imshow("Imopen Black", opening_black)
+
+	# img, contours, hierarchy = cv2.findContours(opening_black, 
+	# 					cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+	# print(len(contours))
+	# blobsBlack = np.array([])
+	# for i in range(0, len(contours)):
+	# 	try:
+	# 		cnt = contours[i]
+	# 		M = cv2.moments(cnt)
+	# 		# print(M)
+	# 		cx = int(M['m10']/M['m00'])
+	# 		cy = int(M['m01']/M['m00'])
+	# 		# print(cx, cy)
+	# 		if i == 0:
+	# 			blobsBlack = np.concatenate((blobsBlack, np.array([cx,cy])))
+	# 		else:
+	# 			blobsBlack = np.vstack((blobsBlack, np.array([cx,cy])))
+			
+	# 	except ZeroDivisionError as detail:
+	# 		print ('Handling error:', detail)
+
+	# print(blobsBlack)
+
+	position = np.array([0.,0.])
+	# if want to get the position with decimals, run following code
+	x = blobsWhite
+
+	listodd = x[::2]
+	listeven = x[1::2]
+
+	thresh = 1
+
+
+	for i in range(len(listodd)):
+		for j in range (len(listeven)):
+			if LA.norm(listodd[i] - listeven[j]) < thresh:
+				position = np.vstack((position, listodd[i]))
+
+
+	position = np.delete(position, 0, 0)
+	# print(position)
+
+
+
+	# x = x.astype(int)
+	# index = DataFrame(x).duplicated().values
+
+	# else, run that:
+	# index = DataFrame(blobsWhite).duplicated().values
+
+	# searching the repeated elements, if element repeating, it will
+	# be one of the points' position
+	# for i in range(index.shape[0]):
+	# 	if index[i] == True:
+	# 		position = np.vstack((position, blobsWhite[i]))
+			# print(blobsWhite[i])
+
+
+	# position = np.delete(position, 0, 0)
+
+	# Using convexHull method to reorder the positions
+	hull = ConvexHull(position)
+	temp = position.copy()
+	for i in range(0,4):
+		position[i-1] = temp[hull.vertices[i]]
+	# print(position)
+
+	# The fllowing code may helpful in the future.
+	# print("The following is the position of black bolobs in the image:")
+	# index = DataFrame(blobsBlack).duplicated().values
+	# for i in range(index.shape[0]):
+	# 	if index[i] == True:
+	# 		print(blobsBlack[i])
+
+	return position
